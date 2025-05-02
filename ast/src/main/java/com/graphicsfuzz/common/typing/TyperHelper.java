aebbbc43840409df3a1e619f873d54fb46156d65
Zheyuan Chen <sephirotheca17@gmail.com>
2025-05-02 15:59:25 -0700

    format

--- ast/src/main/java/com/graphicsfuzz/common/typing/TyperHelper.java
+++ ast/src/main/java/com/graphicsfuzz/common/typing/TyperHelper.java
@@ -16,11 +16,10 @@
 
 package com.graphicsfuzz.common.typing;
 
-import com.graphicsfuzz.common.ast.decl.ArrayInfo;
+// import com.graphicsfuzz.common.ast.decl.ArrayInfo;
 // import com.graphicsfuzz.common.ast.decl.ArrayInfo;
 import com.graphicsfuzz.common.ast.decl.FunctionPrototype;
-import com.graphicsfuzz.common.ast.expr.IntConstantExpr;
-import com.graphicsfuzz.common.ast.type.ArrayType;
+// import com.graphicsfuzz.common.ast.expr.IntConstantExpr;
 // import com.graphicsfuzz.common.ast.type.ArrayType;
 import com.graphicsfuzz.common.ast.type.BasicType;
 import com.graphicsfuzz.common.ast.type.QualifiedType;
@@ -37,7 +36,6 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 // import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
@@ -916,12 +914,7 @@ public final class TyperHelper {
             Collections.singletonList(TypeQualifier.INOUT_PARAM)), t, t);
 
           
-        addBuiltin(builtinsForVersion, "atomicLoad", t, 
-            new QualifiedType(
-              new ArrayType(t, new ArrayInfo(
-                  Collections.singletonList(Optional.of(new IntConstantExpr("1"))))
-              ),
-              Collections.singletonList(TypeQualifier.READONLY)), t, t, t);
+        addBuiltin(builtinsForVersion, "atomicLoad", t, t, t, t, t);
         // addBuiltin(builtinsForVersion, "atomicStore", VoidType.VOID,
         //     new QualifiedType(t, Collections.singletonList(TypeQualifier.INOUT_PARAM)),
         //     t);
