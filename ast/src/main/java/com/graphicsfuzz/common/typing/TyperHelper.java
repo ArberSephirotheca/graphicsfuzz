bd4d805c39736c03e79c17c2be355d07713841fa
Zheyuan Chen <sephirotheca17@gmail.com>
2025-05-02 15:24:03 -0700

    make arrayinfo empty instead of null

--- ast/src/main/java/com/graphicsfuzz/common/typing/TyperHelper.java
+++ ast/src/main/java/com/graphicsfuzz/common/typing/TyperHelper.java
@@ -16,6 +16,7 @@
 
 package com.graphicsfuzz.common.typing;
 
+import com.graphicsfuzz.common.ast.decl.ArrayInfo;
 import com.graphicsfuzz.common.ast.decl.FunctionPrototype;
 import com.graphicsfuzz.common.ast.type.ArrayType;
 import com.graphicsfuzz.common.ast.type.BasicType;
@@ -33,6 +34,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.stream.Collectors;
@@ -909,8 +911,12 @@ public final class TyperHelper {
         addBuiltin(builtinsForVersion, "atomicCompSwap", t, new QualifiedType(t,
             Collections.singletonList(TypeQualifier.INOUT_PARAM)), t, t);
         addBuiltin(builtinsForVersion, "atomicLoad", t, 
-            new QualifiedType(new ArrayType(t, null),
+            new QualifiedType(new ArrayType(t,
+              new ArrayInfo(Collections.singletonList(Optional.empty()))),
             Collections.singletonList(TypeQualifier.READONLY)), t, t, t);
+        // addBuiltin(builtinsForVersion, "atomicStore", VoidType.VOID,
+        //     new QualifiedType(t, Collections.singletonList(TypeQualifier.INOUT_PARAM)),
+        //     t);
       }
       
     }
