39cb1f30fb135e6480b9d232743860d2d54e0934
Zheyuan Chen <sephirotheca17@gmail.com>
2025-05-02 16:46:49 -0700

    update

--- ast/src/main/java/com/graphicsfuzz/common/typing/TyperHelper.java
+++ ast/src/main/java/com/graphicsfuzz/common/typing/TyperHelper.java
@@ -914,7 +914,7 @@ public final class TyperHelper {
             Collections.singletonList(TypeQualifier.INOUT_PARAM)), t, t);
 
         addBuiltin(builtinsForVersion, "atomicLoad", t, new QualifiedType(t,
-            Collections.singletonList(TypeQualifier.INOUT_PARAM)), t, t, t);
+            Collections.singletonList(TypeQualifier.READONLY)), t, t, t);
         // addBuiltin(builtinsForVersion, "atomicStore", VoidType.VOID,
         //     new QualifiedType(t, Collections.singletonList(TypeQualifier.INOUT_PARAM)),
         //     t);
